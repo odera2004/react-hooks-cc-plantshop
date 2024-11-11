897e0676bfb5176e9763c58d1ecc7b27845eb2ac
odera <eugeneodera59@gmail.com>
2024-11-11 08:34:30 +0300

    add text

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,29 @@
 import React from "react";
+import { useState } from "react";
+import Delete from "./Delete";
+// import {useParams} from "react-router-dom";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const [input, setInput] = useState("");
+  
+  function handleInput() {
+    setInput(!input);
+  }
+ 
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
-    </li>
-  );
-}
+    <li className="card">
+      
+                <img src={plant.image} className="card-img-top" alt={plant.name} />
+         <h5 className="p-2">{`Name: ${plant.name}`}</h5>
+          <h5 className="p-2">{`Price: ${plant.price}`}</h5>
+          {input?(<button className="primary" onClick={handleInput}>In stock</button>)
+          :<button onClick={handleInput}>Out of stock</button> }
+          <Delete plant={plant}/>
+{/* <button onClick={handleDelete} type="button" class="btn btn-dark">Delete</button> */}
+    </li> 
+     
+    
+  )};
+
 
 export default PlantCard;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -2,13 +2,31 @@ import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import { useEffect, useState } from "react";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearchPlant] = useState('');
+
+  const handleSearch = (y) => {
+    setSearchPlant(y);
+  }
+  const plantFilter= plants.filter((plant)=> plant.name.toLowerCase().includes(search.toLowerCase()));
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then((response) => response.json())
+      .then((data) => {
+        setPlants(data);
+       
+      });
+  }, []);
+
+  
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList />
+      <Search search={search} onSearchChange={handleSearch}  />
+      <PlantList plants={plantFilter}/>
     </main>
   );
 }
